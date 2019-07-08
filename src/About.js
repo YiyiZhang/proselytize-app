f33620a7d4472b3667f818197fdb1ee5a17624e1
Yiyi Zhang <yiyiwzhang@gmail.com>
2019-07-07 21:19:37 -0400

    states

---
+++ src/About.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class About extends Component{
+
+state={
+	color: 'std'
+}
+
+	changeColor = () => {
+		if(this.state.color == 'std'){
+			this.setState({color: 'alt'})			
+		} else {
+			this.setState({color: 'std'})
+		}
+	}
+
+	render() {
+		return (
+			<div className="About" onClick={this.changeColor}>
+				<h2 className={this.state.color}>This is about us.</h2>
+			</div>
+		)
+	}
+
+}
+
+export default About;
